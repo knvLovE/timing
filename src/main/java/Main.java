1509171befaf0e248d815eb5f293e9e93370170d
Nikita <rk.konovalov@gmail.com>
2020-02-08 17:40:45 +0300

    work version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
